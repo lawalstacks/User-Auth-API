8c9d6ec6452d598e8f93f43894c5f350326fb912
lawalstacks <quazeemastic@gmail.com>
2024-10-24 04:03:38 +0100

    update

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -4,7 +4,7 @@ import { IUser } from '../interfaces/userInterfaces'; // Import the IUser interf
 // Mongoose Schema
 const UserSchema: Schema<IUser> = new Schema({
     _jid:{type: String},
-    username: { type: String, required: true },
+    username: { type: String, required: true,unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     createdAt: { type: Date, default: Date.now }
